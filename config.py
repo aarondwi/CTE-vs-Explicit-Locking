6a56d485aced2102761fcad8ddb37ec491f8b82f
aaron <aarondwico@gmail.com>
2020-04-26 23:23:58 +0700

    add docker-compose for easier testing

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-db_username="postgres"
-db_password="postgres"
+db_username="user"
+db_password="pass"
 db_ip="localhost"
-db_name="postgres"
+db_name="db"
 db_port=5432
 db_pool_minconn=1
 db_pool_maxconn=10
